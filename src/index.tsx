7f27d1b662eaf8136f7b4f9fbf3e79b68055808e
vincent178 <vincent.007.cn@gmail.com>
2017-11-15 20:36:27 +0800

    add typescript and create-react-typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import * as React from 'react';
 import { Provider } from 'react-redux';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router'
 
 import createStore from '../common/store';
